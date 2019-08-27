7c092fc77032ae41b86644597d5a68983345f38e
NKV-88 <sysadm86@gmail.com>
2019-08-27 10:36:52 +0500

    2 задание второго урока выполнил

---
+++ Task2 (2 задание)/main.js
@@ -0,0 +1,16 @@
+export class Developer {
+    constructor  (a,b,c){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+    calc (d) {
+        return a+b+c;
+    }
+    info (){
+        console.log(d);
+    }
+    
+}
+
+export let test= `1`;
---
+++ Task2 (2 задание)/main1.js
@@ -0,0 +1,8 @@
+import {Developer,test} from './main';
+
+let newDev = new Developer(5,2,1);
+
+newDev.calc();
+newDev.info();
+
+console.log(test);
